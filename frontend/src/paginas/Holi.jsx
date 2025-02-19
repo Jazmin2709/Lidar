0c05735f884d64543c5ec73aaef485eb16c51a39
Jazmin2709 <cielojvargas@gmail.com>
2025-02-19 18:56:47 -0500

    Cambio
    Cambie el titulo del archivo holi

--- frontend/src/paginas/Holi.jsx
+++ frontend/src/paginas/Holi.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Holi() {
   return (
     <div className='border border-4 rounded p-4'>
-      <h1>Holi, soy un componente de React!</h1>
+      <h1>TENGO SUEÑO</h1>
     </div>
   )
 }
