e79a3df032ae33e4ecc8a5d76ed9175a688da47b
Jazmin2709 <cielojvargas@gmail.com>
2025-12-05 19:38:19 -0500

    Update imagenes.js

--- backend/routes/imagenes.js
+++ backend/routes/imagenes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { upload, validateImagen } = require('../middlewares/validateImagenes');
-const imagenesController = require('../controllers/ImagenesController');
+const ImagenesController = require('../controllers/ImagenesController');
 
 const router = express.Router();
 
-router.post('/subir', upload.single('foto'), validateImagen, imagenesController.subirImagen);
+router.post('/subir', upload.single('foto'), validateImagen, ImagenesController.subirImagen);
 
 router.post('/eliminar/:public_id', ImagenesController.eliminarImagen);
 
